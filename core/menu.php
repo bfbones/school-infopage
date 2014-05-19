7c9552bdb267cb5dbf4d14971d56c23b23415915
Konrad Mallok <kony@kony-SERVER.fritz.box>
2014-05-19 20:27:45 +0200

    patched

--- core/menu.php
+++ core/menu.php
@@ -1,6 +1,6 @@
 <?php
 echo '<ul>
-<li><a href="index.php"><span>Übersicht</span></a></li>
+<li><a href="index.php"><span>&Uuml;bersicht</span></a></li>
 <li><a href="plan.php?gr=1"><span>Stundenplan GR1</span></a></li>
 <li><a href="plan.php?gr=2"><span>Stundenplan GR2</span></a></li>
 <li><a href="ftp://'.$conf['ftp_host'].':'.$conf['ftp_port'].'" target="_blank"><span>Filebrowser (FTP)</span></a></li>
